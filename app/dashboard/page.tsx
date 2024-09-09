8fa7828aa59871cd31d207855d5a20477a9d1688
tyson-tyler <benword25@gmail.com>
2024-09-09 10:07:30 +0530

    sdfaa

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -14,7 +14,7 @@ export default async function page() {
       {/* <Image src={session?.user?.image} alt="hello" width={20} height={20} /> */}
       {/* <h1>{session?.user?.email}</h1> */}
       <img
-        src={session?.user?.image}
+        src={`${session?.user?.image}`}
         width={40}
         height={40}
         className="rounded-md absolute top-3 right-16 lg:right-20"
