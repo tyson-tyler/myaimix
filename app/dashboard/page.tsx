346100c2f473efd598afc0345bb26363a8a1c2f6
tyson-tyler <benword25@gmail.com>
2024-09-09 19:50:25 +0530

    asfd

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import Topbar from "./components/layout/topbar/topbar";
+
 import { auth } from "../auth";
+import Image from "next/image";
 
 export default async function page() {
   const session = await auth();
@@ -12,10 +13,11 @@ export default async function page() {
       </h1>
       {/* <Image src={session?.user?.image} alt="hello" width={20} height={20} /> */}
       {/* <h1>{session?.user?.email}</h1> */}
-      <img
+      <Image
         src={`${session?.user?.image || "https://cdn.waifu.im/6929.jpg"}`}
         width={40}
         height={40}
+        alt="hello"
         className="rounded-md w-10 h-10 absolute top-3 object-cover right-16 lg:right-20"
       />
       {/* Content goes here */}
