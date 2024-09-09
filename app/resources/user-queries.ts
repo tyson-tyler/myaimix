dba800d9e0a7e14abd3d44b070f12ae2a0d250e6
tyson-tyler <benword25@gmail.com>
2024-09-09 15:19:30 +0530

    adsf

--- app/resources/user-queries.ts
+++ app/resources/user-queries.ts
@@ -1,14 +1,14 @@
+"use server";
+
 import db from "../drizzle";
 import { lower, users } from "../drizzle/schema";
 import { eq } from "drizzle-orm";
 
-export const findUserEmail = async (email: string) => {
+export const findUserEmail = async (
+  email: string
+): Promise<typeof users.$inferSelect | null> => {
   const user = await db
-    .select({
-      email: users.email,
-      name: users.name,
-      id: users.id,
-    })
+    .select()
     .from(users)
     .where(eq(lower(users.email), email.toLowerCase()))
     .then((res) => res[0] ?? null);
